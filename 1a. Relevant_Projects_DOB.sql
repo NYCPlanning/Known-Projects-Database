9711b4627eaf88157755b58ac617b60666d6341a
mshapiro1 <45976493+mshapiro1@users.noreply.github.com>
2019-04-09 17:59:58 -0400

    Update 1a. Relevant_Projects_DOB.sql

--- 1a. Relevant_Projects_DOB.sql
+++ 1a. Relevant_Projects_DOB.sql
@@ -4,19 +4,23 @@ SCRIPT: Preparing DOB's devdb_housing_pts dataset for joining
 START DATE: 1/10/2019
 COMPLETION DATE: 1/10/2019
 Source file: "G:\03. Schools Planning\01_Inputs to SCA CP\Housing pipeline\00_Data\
-			  Jan 2019 SCA Housing Pipeline\Raw Data\DOB\devdb_housing_pts.csv"
+		Jan 2019 SCA Housing Pipeline\Raw Data\DOB\devdb_housing_pts.csv"
 *************************************************************************************************************************************************************************************/
-
 
 /************************************************************************************************************************************************************************************
 METHODOLOGY:
 1. Remove inactive/closed and completed projects.
 2. Remove non-residential projects.
-3. Rearrange fields.
+3. Redefine "status" field to identify Partial Complete projects
+4. Redefine "status" field to identify Complete NB projects without COs as Permit Issued
+5. Rearrange fields.
 *************************************************************************************************************************************************************************************/
-
-
-with dob_2018_sca_inputs_ms as
+/***********************************RUN IN CARTO BATCH*****************************/
+select
+	*
+into
+	dob_2018_sca_inputs_ms 
+from
 (
 select
 	the_geom,
@@ -26,13 +30,20 @@ select
 	boro 										as borough,
 	occ_init,
 	occ_prop,
-	case when 
-			job_type = 'New Building' and 
-			status = 'Complete' and 
-			co_latest_certtype = 'T- TCO' and
+	case 
+		when 	/*Creating a Partial Complete status*/
+			job_type = 'New Building' 	and 
+			status = 'Complete' 		and 
+			co_latest_certtype = 'T- TCO' 	and
 			cast(co_latest_units as double precision)/cast(units_net as double precision) < .8 
-								then 'Partial Complete'  
-								else status end
+											then 'Partial Complete'
+		when	/*~100 NB jobs have been labeled as complete without associated COs. Reverting to status
+			  'Permit Issued'*/	
+			job_type = 'New Building' 	and
+			status = 'Complete'		and
+			co_latest_units is null		
+											then 'Permit Issued'
+											else status end
 											as status,
 	status_date 									as most_recent_status_date,
 	right(status_a,4) 								as pre_filing_year,
@@ -56,8 +67,8 @@ select
 from 
 	capitalplanning.devdb_housing_pts_2019_02_08 
 where
-	status <>'Withdrawn' and 
-	x_inactive = 'false' and /*Non-permitted job w/o update since last two years ago*/
+	status 		<>'Withdrawn' 	and 
+	x_inactive 	= 'false' 	and /*Non-permitted job w/o update since two years ago*/
 	(
 		upper(occ_init) like '%RESIDENTIAL%' or
 		upper(occ_prop) like '%RESIDENTIAL%'
@@ -65,6 +76,9 @@ where
 		projects, limited by above fields, by 21 observations (875 units_net).*/
 order by
 	job_number
-)
+) as dob_2018_sca_inputs_ms
 
-/*Create dataset from query called dob_2018_sca_inputs_ms */
+/***********************************RUN IN REGULAR CARTO*****************************/
+
+select cdb_cartodbfytable('capitalplanning', 'dob_pipeline_ms')
+
